0c28c9aead3de3e865dac5763de127f87689468f
Purvee Agrawal <purvee.agrawal@gmail.com>
2024-05-15 16:01:25 +0530

    Create longestWord.js
    Given an array of strings words representing an English Dictionary, return the longest word in words that can be built one character at a time by other words in words.
    
    If there is more than one possible answer, return the longest word with the smallest lexicographical order. If there is no answer, return the empty string.
    
    Note that the word should be built from left to right with each additional character being added to the end of a previous word.

---
+++ longestWord.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestWord = function(words) {
+    let sortedWords = words.sort()
+    let wordMap = new Map();
+    sortedWords.forEach((word)=>{
+        if(word.length == 1){
+            wordMap.set(word,word.length);
+        }
+        else{
+            if(wordMap.get(word.substr(0,word.length-1))){
+                wordMap.set(word,word.length);
+            }
+        }
+    })
+    let largestValue = 0;
+    let largestValues = [];
+    for(let [key,value] of wordMap){
+       if(value > largestValue){
+            largestValue = value;
+        }
+    }
+
+    for(let [key,value] of wordMap){
+       if(value == largestValue){
+        largestValues.push(key);
+       }
+    }
+
+    return largestValues.sort()[0] ? largestValues.sort()[0] : ""
+};
